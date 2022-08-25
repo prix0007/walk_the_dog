62737ad63bb2a4b478ead15592832b6cf312ec16
Prince Anuragi <prince@uniris.io>
2022-08-25 13:08:28 +0530

    Added RHB Move Animations

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,20 @@
-use std::error;
-use std::rc::Rc;
-use std::sync::Mutex;
-
-use rand::prelude::*;
-use rand::thread_rng;
+use engine::GameLoop;
+use engine::KeyState;
+use engine::Point;
 use serde::Deserialize;
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
-use web_sys::console;
+use web_sys::HtmlImageElement;
 
 use std::collections::HashMap;
 
+#[macro_use]
+mod browser;
+mod engine;
+
+use crate::engine::{Game, Rect, Renderer};
+use anyhow::Result;
+use async_trait::async_trait;
 // When the `wee_alloc` feature is enabled, this uses `wee_alloc` as the global
 // allocator.
 //
@@ -19,21 +23,115 @@ use std::collections::HashMap;
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
-#[derive(Deserialize, Debug)]
-struct Sheet {
+#[derive(Deserialize)]
+struct SheetRect {
+    x: i16,
+    y: i16,
+    w: i16,
+    h: i16,
+}
+#[derive(Deserialize)]
+struct Cell {
+    frame: SheetRect,
+}
+#[derive(Deserialize)]
+pub struct Sheet {
     frames: HashMap<String, Cell>,
 }
-
-#[derive(Deserialize, Debug)]
-struct Rect {
-    x: u16,
-    y: u16,
-    w: u16,
-    h: u16,
+pub struct WalkTheDog {
+    image: Option<HtmlImageElement>,
+    sheet: Option<Sheet>,
+    frame: u8,
+    position: Point,
 }
-#[derive(Deserialize, Debug)]
-struct Cell {
-    frame: Rect,
+
+#[async_trait(?Send)]
+impl Game for WalkTheDog {
+    async fn initialize(&self) -> Result<Box<dyn Game>> {
+        let sheet = browser::fetch_json("rhb.json").await?.into_serde()?;
+        let image = Some(engine::load_image("rhb.png").await?);
+        Ok(Box::new(WalkTheDog {
+            image,
+            sheet,
+            frame: self.frame,
+            position: self.position,
+        }))
+    }
+    fn update(&mut self, keystate: &KeyState) {
+        if self.frame < 23 {
+            self.frame += 1;
+        } else {
+            self.frame = 0;
+        }
+
+        let mut velocity = self.position.clone();
+
+        if keystate.is_pressed("ArrowDown") {
+            velocity.y += 3;
+        }
+
+        if keystate.is_pressed("ArrowUp") {
+            velocity.y -= 3;
+        }
+
+        if keystate.is_pressed("ArrowLeft") {
+            velocity.x -= 3;
+        }
+        if keystate.is_pressed("ArrowRight") {
+            velocity.x += 3;
+        }
+
+        self.position.x = velocity.x;
+        self.position.y = velocity.y;
+    }
+    fn draw(&self, renderer: &Renderer) {
+        let current_sprite = (self.frame / 3) + 1;
+        // log!("{}", current_sprite);
+        let frame_name = format!("Run ({}).png", current_sprite);
+        // log!("{}",frame_name);
+        let sprite = self
+            .sheet
+            .as_ref()
+            .and_then(|sheet| sheet.frames.get(&frame_name))
+            .expect("Cell not found");
+        renderer.clear(&Rect {
+            x: 0.0,
+            y: 0.0,
+            width: 600.0,
+            height: 600.0,
+        });
+
+        self.image.as_ref().map(|image| {
+            renderer
+                .draw_image(
+                    &image,
+                    &Rect {
+                        x: sprite.frame.x.into(),
+                        y: sprite.frame.y.into(),
+                        width: sprite.frame.w.into(),
+                        height: sprite.frame.h.into(),
+                    },
+                    &Rect {
+                        x: self.position.x.into(),
+                        y: self.position.y.into(),
+                        width: sprite.frame.w.into(),
+                        height: sprite.frame.h.into(),
+                    },
+                )
+                .expect("Expected to draw Image");
+        });
+    }
+}
+
+impl WalkTheDog {
+    pub fn new() -> Self {
+        WalkTheDog {
+            image: None,
+            sheet: None,
+            frame: 0,
+            position: Point { x: 0, y: 0 },
+        }
+    }
 }
 
 // This is like the `main` function, except for JavaScript.
@@ -44,147 +142,11 @@ pub fn main_js() -> Result<(), JsValue> {
     #[cfg(debug_assertions)]
     console_error_panic_hook::set_once();
 
-    let window = web_sys::window().unwrap();
-    let document = window.document().unwrap();
-
-    let canvas = document
-        .get_element_by_id("canvas")
-        .unwrap()
-        .dyn_into::<web_sys::HtmlCanvasElement>()
-        .unwrap();
-
-    let context = canvas
-        .get_context("2d")
-        .unwrap()
-        .unwrap()
-        .dyn_into::<web_sys::CanvasRenderingContext2d>()
-        .unwrap();
-
-    wasm_bindgen_futures::spawn_local(async move {
-        let (success_tx, success_rx) = futures::channel::oneshot::channel::<Result<(), JsValue>>();
-        let success_tx = Rc::new(Mutex::new(Some(success_tx)));
-        let error_tx = Rc::clone(&success_tx);
-        let image = web_sys::HtmlImageElement::new().unwrap();
-        let callback = Closure::once(move || {
-            if let Some(success_tx) = success_tx.lock().ok().and_then(|mut opt| opt.take()) {
-                success_tx.send(Ok(()));
-            }
-        });
-        let error_callback = Closure::once(move |err| {
-            if let Some(error_tx) = error_tx.lock().ok().and_then(|mut opt| opt.take()) {
-                error_tx.send(Err(err));
-            }
-        });
-
-        let json = fetch_json("rhb.json")
+    browser::spawn_local(async move {
+        let game = WalkTheDog::new();
+        GameLoop::start(game)
             .await
-            .expect("Could not fetch rhb.json");
-        let sheet: Sheet = json
-            .into_serde()
-            .expect("Could not convert rhb.json into a Sheet structure");
-
-        image.set_onload(Some(callback.as_ref().unchecked_ref()));
-        image.set_onerror(Some(error_callback.as_ref().unchecked_ref()));
-
-        // image.set_src("Idle (1).png");
-
-        image.set_src("rhb.png");
-        success_rx.await;
-
-        // context.draw_image_with_html_image_element(&image, 0.0, 0.0);
-
-        sierpinski(
-            &context,
-            [(300.0, 0.0), (0.0, 600.0), (600.0, 600.0)],
-            (0, 255, 0),
-            6,
-        );
-
-        let sprite = sheet.frames.get("Run (1).png").expect("Cell not found");
-        context.draw_image_with_html_image_element_and_sw_and_sh_and_dx_and_dy_and_dw_and_dh(
-            &image,
-            sprite.frame.x.into(),
-            sprite.frame.y.into(),
-            sprite.frame.w.into(),
-            sprite.frame.h.into(),
-            300.0,
-            300.0,
-            sprite.frame.w.into(),
-            sprite.frame.h.into(),
-        );
+            .expect("Could not start game loop");
     });
     Ok(())
-}
-
-async fn fetch_json(json_path: &str) -> Result<JsValue, JsValue> {
-    let window = web_sys::window().unwrap();
-    let resp_value = wasm_bindgen_futures::JsFuture::from(window.fetch_with_str(json_path)).await?;
-    let resp: web_sys::Response = resp_value.dyn_into()?;
-
-    wasm_bindgen_futures::JsFuture::from(resp.json()?).await
-}
-
-fn midpoint(point_1: (f64, f64), point_2: (f64, f64)) -> (f64, f64) {
-    ((point_1.0 + point_2.0) / 2.0, (point_1.1 + point_2.1) / 2.0)
-}
-
-fn sierpinski(
-    context: &web_sys::CanvasRenderingContext2d,
-    points: [(f64, f64); 3],
-    color: (u8, u8, u8),
-    depth: u8,
-) {
-    draw_triangle(&context, points, color);
-
-    let depth = depth - 1;
-
-    let [top, left, right] = points;
-
-    if depth > 0 {
-        let mut rng = thread_rng();
-        let next_color = (
-            rng.gen_range(0..255),
-            rng.gen_range(0..255),
-            rng.gen_range(0..255),
-        );
-        let left_middle = midpoint(top, left);
-        let right_middle = midpoint(top, right);
-        let bottom_middle = midpoint(left, right);
-        sierpinski(
-            &context,
-            [top, left_middle, right_middle],
-            next_color,
-            depth,
-        );
-        sierpinski(
-            &context,
-            [left_middle, left, bottom_middle],
-            next_color,
-            depth,
-        );
-        sierpinski(
-            &context,
-            [right_middle, bottom_middle, right],
-            next_color,
-            depth,
-        );
-    }
-}
-
-fn draw_triangle(
-    context: &web_sys::CanvasRenderingContext2d,
-    points: [(f64, f64); 3],
-    color: (u8, u8, u8),
-) {
-    let [top, left, right] = points;
-    context.move_to(top.0, top.1); // top of triangle
-    context.begin_path();
-    context.line_to(left.0, left.1); // bottom left of triangle
-    context.line_to(right.0, right.1); // bottom right of triangle
-    context.line_to(top.0, top.1); // back to top of triangle
-    context.close_path();
-    context.stroke();
-    let color_str = format!("rgb({}, {}, {})", color.0, color.1, color.2);
-    context.set_fill_style(&wasm_bindgen::JsValue::from_str(&color_str));
-    context.fill();
 }
